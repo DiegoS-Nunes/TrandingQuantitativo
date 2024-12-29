c4a8172aa4e5f1c11b0093ad33efeddc360dfeb9
Diego Nunes <diego.s.nunes213@gmail.com>
2024-12-29 17:53:01 -0300

    Extração de dados da CVM corrigido, pivoteando, eliminando colunas nulas e salvando na pasta com nomenclatura correta.

--- BD/bd.py
+++ BD/bd.py
@@ -46,15 +46,15 @@ class bd():
             print("Sucesso ao logar no Meta Trader 5")
 
         if not os.path.isdir('ohlc'):
-            os.mkdir('ohlc')
+            os.makedirs('ohlc')
             for timeframe_dir in timeframe_dict.keys():
                 try:
-                    os.mkdir(f'ohlc/{timeframe_dir}')
+                    os.makedirs(f'ohlc/{timeframe_dir}')
                 except Exception as e:
                     print(f"Erro ao criar as pastas de timeframe: {e}")
         if not os.path.isdir('ticks'):
             try:
-                os.mkdir('ticks')
+                os.makedirs('ticks')
                 print("Sucesso ao criar o diretório 'ticks'!")
             except Exception as e:
                 print(f"Erro ao criar o diretório 'ticks': {e}")
@@ -163,11 +163,19 @@ class bd():
         
         return sorted(empresas)    
 
-    def processar_dados_empresa(self, empresa):
+    def update_fundamentus(self, empresa):
         BASE_URL = "https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/DFP/DADOS/"
         BASE_DIR = "./fundamentus"
-        
+
+        if empresa.endswith('.'):
+            empresa_corrigida = empresa[:-1]
+
+        # Cria o diretório da empresa dentro da pasta fundamentus
+        empresa_dir = os.path.join(BASE_DIR,empresa_corrigida)
+        os.makedirs(empresa_dir, exist_ok=True)
+
         response = requests.get(BASE_URL)
+
         if response.status_code != 200:
             print("Erro ao acessar o site da CVM.")
             return
@@ -176,86 +184,125 @@ class bd():
         
         zip_files = [link.get('href') for link in soup.find_all('a') if link.get('href').endswith('.zip')]
         
-        # DataFrame temporário para acumular os dados
-        df_final = pd.DataFrame()
+        # Tipos de relatórios a serem processados
+        relatorios = ['DRE', 'BPA', 'BPP', 'DFC']
         
-        try:
-            for zip_file_name in zip_files:
-                url = f"{BASE_URL}{zip_file_name}"
-                response = requests.get(url)
-                if response.status_code == 200:
-                    with zipfile.ZipFile(BytesIO(response.content)) as zip_file:
-                        for csv_name in zip_file.namelist():
-                            # Verifica se o arquivo CSV é o DRE
-                            if csv_name.endswith('.csv') and 'DRE' in csv_name:
-                                with zip_file.open(csv_name) as f:
-                                    df = pd.read_csv(f, sep=';', encoding='ISO-8859-1')
-                                
-                                # Normaliza o nome da empresa para comparação
-                                df_empresa = df[df['DENOM_CIA'].apply(lambda x: re.sub(r'\s+', ' ', x.strip()).lower()) == re.sub(r'\s+', ' ', empresa.strip()).lower()]
-                                
-                                if not df_empresa.empty:
-                                    # Extrai o ano do nome do arquivo ZIP
-                                    ano = int(zip_file_name.split('_')[-1].split('.')[0])
-                                    
-                                    # Adiciona a coluna 'Ano' ao DataFrame
-                                    df_empresa = df_empresa.copy()  # Evita o SettingWithCopyWarning
-                                    df_empresa.loc[:, 'Ano'] = ano
-                                    
-                                    # Adiciona a coluna 'Tipo' ao DataFrame
-                                    tipo_consolidacao = 'con' if 'con' in csv_name else 'ind'
-                                    df_empresa.loc[:, 'Tipo'] = tipo_consolidacao
-                                    
-                                    # Filtra apenas o último exercício
-                                    df_empresa = df_empresa[df_empresa['ORDEM_EXERC'] == 'ÚLTIMO']
-                                    
-                                    # Verifica se há dados após a filtragem
-                                    if df_empresa.empty:
-                                        print(f"Nenhum dado encontrado para o último exercício da empresa {empresa}.")
-                                        continue
-                                    
-                                    # Pivotar o DataFrame para transformar as contas em colunas
-                                    df_pivot = df_empresa.pivot_table(
-                                        index=['Ano', 'Tipo', 'CNPJ_CIA', 'DT_REFER', 'DENOM_CIA', 'ESCALA_MOEDA', 'DT_INI_EXERC', 'DT_FIM_EXERC'],
-                                        columns='DS_CONTA',
-                                        values='VL_CONTA',
-                                        aggfunc='first'  # Assume que cada combinação de índice e coluna tem apenas um valor
-                                    ).reset_index()
-                                    
-                                    # Renomear o índice para facilitar a leitura
-                                    df_pivot.columns.name = None  # Remove o nome das colunas (DS_CONTA)
-                                    df_pivot = df_pivot.rename_axis(None, axis=1)
-                                    
-                                    # Concatena os dados no DataFrame final
-                                    df_final = pd.concat([df_final, df_pivot], ignore_index=True)
-                                    print('Concatenado')
-                else:
-                    print(f"Erro ao baixar o arquivo ZIP: {zip_file_name}")
-            
-            # Verifica se há dados no DataFrame final
-            if df_final.empty:
-                print(f"Nenhum dado encontrado para a empresa {empresa}.")
-                return
-            
+        for relatorio in relatorios:
             # Define o caminho do arquivo Parquet
-            parquet_path = os.path.join(BASE_DIR, f"{empresa}_DRE.parquet")
+            parquet_path = os.path.join(empresa_dir, f"{empresa}_{relatorio}.parquet")
             
-            # Salva o DataFrame final em Parquet
-            self.salvar_parquet(parquet_path, df_final)
-            print(f"Dados de {empresa} salvos em Parquet com sucesso.")
-            print(f"FINALIZADO!")
-        except Exception as e:
-            print(f"Erro ao obter fundamentos da empresa {empresa}: {str(e)}")
-   
+            # Verifica se o arquivo Parquet já existe
+            if os.path.exists(parquet_path):
+                # Carrega os dados existentes
+                df_existente = pd.read_parquet(parquet_path)
+                # Extrai os anos já presentes no arquivo Parquet
+                anos_existentes = df_existente['ano'].unique()
+                print(f"Anos existentes para {relatorio}: {anos_existentes} \n Pulando...")
+            else:
+                # Se o arquivo não existir, cria um DataFrame vazio
+                df_existente = pd.DataFrame()
+                anos_existentes = []
+            
+            # DataFrame temporário para acumular os dados
+            df_final = pd.DataFrame()
+            
+            try:
+                for zip_file_name in zip_files:
+                    # Extrai o ano do nome do arquivo ZIP
+                    ano = int(zip_file_name.split('_')[-1].split('.')[0])
+                    
+                    # Verifica se o ano já está presente no arquivo Parquet
+                    if ano in anos_existentes:
+                        continue  # Pula para o próximo arquivo ZIP
+                    
+                    url = f"{BASE_URL}{zip_file_name}"
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        with zipfile.ZipFile(BytesIO(response.content)) as zip_file:
+                            for csv_name in zip_file.namelist():
+                                # Verifica se o arquivo CSV é do relatório atual
+                                if csv_name.endswith('.csv') and relatorio in csv_name:
+                                    with zip_file.open(csv_name) as f:
+                                        df = pd.read_csv(f, sep=';', encoding='ISO-8859-1')
+                                    
+                                    # Normaliza o nome da empresa para comparação
+                                    df_empresa = df[df['DENOM_CIA'].apply(lambda x: re.sub(r'\s+', ' ', x.strip()).lower()) == re.sub(r'\s+', ' ', empresa.strip()).lower()]
+                                    
+                                    if not df_empresa.empty:
+                                        # Evita o SettingWithCopyWarning
+                                        df_empresa = df_empresa.copy()
+                                        
+                                        # Adiciona a coluna 'Ano' ao DataFrame
+                                        df_empresa.loc[:, 'ano'] = ano
+                                        
+                                        # Adiciona a coluna 'Tipo' ao DataFrame
+                                        tipo_consolidacao = 'con' if 'con' in csv_name else 'ind'
+                                        df_empresa.loc[:, 'tipo'] = tipo_consolidacao
+                                        
+                                        # Filtra apenas o último exercício
+                                        df_empresa = df_empresa[df_empresa['ORDEM_EXERC'] == 'ÚLTIMO']
+                                        
+                                        # Verifica se há dados após a filtragem
+                                        if df_empresa.empty:
+                                            print(f"Nenhum dado encontrado para o último exercício da empresa {empresa} no ano {ano}.")
+                                            continue
+                                        
+                                        # Define as colunas do índice com base no relatório
+                                        if relatorio in ['BPA', 'BPP']:
+                                            # Para BPA e BPP, não inclui 'DT_INI_EXERC' no índice
+                                            index_cols = ['ano', 'tipo', 'CNPJ_CIA', 'DT_REFER', 'DENOM_CIA', 'ESCALA_MOEDA', 'DT_FIM_EXERC']
+                                        else:
+                                            # Para DRE e DFC, inclui 'DT_INI_EXERC' no índice
+                                            index_cols = ['ano', 'tipo', 'CNPJ_CIA', 'DT_REFER', 'DENOM_CIA', 'ESCALA_MOEDA', 'DT_INI_EXERC', 'DT_FIM_EXERC']
+                                        
+                                        # Pivotar o DataFrame para transformar as contas em colunas
+                                        df_pivot = df_empresa.pivot_table(
+                                            index=index_cols,
+                                            columns='DS_CONTA',
+                                            values='VL_CONTA',
+                                            aggfunc='first'  # Assume que cada combinação de índice e coluna tem apenas um valor
+                                        ).reset_index()
+                                        
+                                        # Renomear o índice para facilitar a leitura
+                                        df_pivot.columns.name = None  # Remove o nome das colunas (DS_CONTA)
+                                        df_pivot = df_pivot.rename_axis(None, axis=1)
+                                        
+                                        # Concatena os dados no DataFrame final
+                                        df_final = pd.concat([df_final, df_pivot], ignore_index=True)
+                                        print(f"Concatenado dados do relatório {relatorio} para o ano {ano}.")
+                    else:
+                        print(f"Erro ao baixar o arquivo ZIP: {zip_file_name}")
+                    
+                # Verifica se há dados no DataFrame final
+                if df_final.empty:
+                    print(f"Nenhum dado novo de encontrado para a empresa {empresa} no relatório {relatorio} {ano}.")
+                    continue
+                
+                # Concatena os novos dados com os existentes
+                df_final = pd.concat([df_existente, df_final], ignore_index=True)
+                df_final = df.apply(pd.to_numeric, errors='ignore')
+                df_final = df_final.dropna(axis=1, how='all')
+
+                # Salva o DataFrame final em Parquet
+                self.salvar_parquet(parquet_path, df_final)
+                print(f"Dados de {empresa} para o relatório {relatorio} salvos em Parquet com sucesso.")
+            except Exception as e:
+                print(f"Erro ao obter fundamentos da empresa {empresa} para o relatório {relatorio}: {str(e)}")
+            
+                print(f"FINALIZADO!")
+
     def slice(self, type, symbol, initial_date, final_date, timeframe=None):
         path = f'ohlc\\{timeframe}\\{symbol}_{timeframe}.parquet' if type == 'ohlc' else f'ticks\\{symbol}_ticksrange.parquet'
         if not os.path.exists(path):
             print(f"O ativo {symbol} não está registrado, favor criá-lo utilizando a função .update_{type}()")
         else:
             df = pq.ParquetFile(path).read().to_pandas()
-            df['time'] = pd.to_datetime(df['time'], unit='s')
-            df['time_msc'] = pd.to_datetime(df['time_msc'], unit='ms')
-            return df.loc[(df['time'] >= initial_date) & (df['time'] < final_date)]
+            colunas = df.columns.tolist()
+            if 'time' in colunas:
+                df['time'] = pd.to_datetime(df['time'], unit='s')
+                if 'time_msc' in colunas:
+                    df['time_msc'] = pd.to_datetime(df['time_msc'], unit='ms')
+                return df.loc[(df['time'] >= initial_date) & (df['time'] < final_date)]
 
     def read_ohlc(self, symbol, timeframe, initial_date=datetime(2012, 1, 1), final_date=datetime.now()):
         return self.slice('ohlc', symbol, initial_date, final_date, timeframe)
@@ -263,8 +310,11 @@ class bd():
     def read_ticks(self, symbol, initial_date=datetime(1970, 1, 1), final_date=datetime.now()):
         return self.slice('ticks', symbol, initial_date, final_date)
 
-    def read_fundamentus(self, empresa):
-        path = f'fundamentus\\{empresa}_DRE.parquet'
+    def read_fundamentus(self, empresa, relatorio='DRE'):
+        if empresa.endswith('.'):
+            empresa_corrigida = empresa[:-1]
+        
+        path = f'fundamentus\\{empresa_corrigida}\\{empresa}_{relatorio}.parquet'
         df = pq.ParquetFile(path).read().to_pandas()
         return df
     
